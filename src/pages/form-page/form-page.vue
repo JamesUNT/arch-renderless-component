60f4feaf2da22c8f12dbfe3d9573d4edfde304ef
JamesUNT <thiagoaugusto.inc@gmail.com>
2025-10-19 19:01:15 -0300

    feat: Simulação de uma requisição para o componente

--- src/pages/form-page/form-page.vue
+++ src/pages/form-page/form-page.vue
@@ -1,7 +1,18 @@
 <template>
-  <modal-form />
+  <modal-form :data="dataFromPage" />
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import modalForm from '@/modals/modal-form/modal-form.vue'
+
+const dataFromPage = ref({ nome: '', email: '' })
+
+onMounted(async () => {
+  console.log('Iniciando a operação de busca de dados... ')
+  await new Promise((resolve) => setTimeout(resolve, 3000)) // Simula um processamento inicial
+  dataFromPage.value.nome = 'Thiago'
+  dataFromPage.value.email = 'thiago@gmail.com'
+  console.log('Operação concluída: ', { ...dataFromPage.value })
+})
 </script>
